376174b1d5595f570791e3f70e47bc9618434c5c
mittjobbkenny <mittjobbkenny@gmail.com>
2023-03-29 12:38:58 +0200

    commit 5

--- src/App.js
+++ src/App.js
@@ -1,27 +1,16 @@
 import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
-  const title = 'Welcome to the new blog';
-  const likes = 50;
-  const person = { name: 'Yoshi', age: 50 };
-  const link = 'http://www.google.com';
-
-  return (
-    <div className="App">
-      <div className="content">
-        <h1>{title}</h1>
-        <p>Liked {likes} times</p>
-        <p>{person.name}</p>
-
-        <p>{10}</p>
-        <p>{"hello, ninjas"}</p>
-        <p>{[1, 2, 3, 4, 5]}</p>
-        <p>{Math.random() * 10}</p>
-
-        <a href={link}>Google Site</a>
-      </div>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Navbar />
+            <div className="content">
+                <Home />
+            </div>
+        </div>
+    );
 }
 
 export default App;
